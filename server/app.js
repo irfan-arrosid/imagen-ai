75103417e8571a12b6f5cc38e31a340d309a85c0
irfan-arrosid <irfanarrosid4@gmail.com>
2023-04-23 21:25:09 +0700

    Feat: add OpenAI router to generate image from prompt

--- server/app.js
+++ server/app.js
@@ -3,12 +3,17 @@ import * as dotenv from 'dotenv'
 import cors from 'cors'
 
 import connectDB from './db/database.js'
+import postRoutes from './routes/postRoutes.js'
+import imaginRoutes from './routes/imagenRoutes.js'
 
 dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
+
+app.use('/api/v1/posts', postRoutes)
+app.use('/api/v1/imagin', imaginRoutes)
 
 app.get('/', async (req, res) => {
     res.send('Hello, World!')
