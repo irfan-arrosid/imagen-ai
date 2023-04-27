301a800b68271d26b182b05ee43d1b8848626b94
irfan-arrosid <irfanarrosid4@gmail.com>
2023-04-27 23:37:12 +0700

    Feat: add handleSearchChange function to filtering result by searchText input

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,9 +13,11 @@ const RenderCards = ({ data, title }) => {
 }
 
 const Home = () => {
-  const [loading, setLoading] = useState(false);
-  const [allPosts, setAllPosts] = useState(null);
-  const [searchText, setSearchText] = useState('');
+  const [loading, setLoading] = useState(false)
+  const [allPosts, setAllPosts] = useState(null)
+  const [searchText, setSearchText] = useState('')
+  const [searchedResults, setSearchedResults] = useState(null)
+  const [searchTimeout, setSearchTimeout] = useState(null)
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -44,6 +46,20 @@ const Home = () => {
     fetchPosts()
   }, [])
 
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout)
+
+    setSearchText(e.target.value)
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResults = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()))
+  
+        setSearchedResults(searchResults)
+      }, 500)
+    )
+  }
+
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
@@ -52,7 +68,14 @@ const Home = () => {
       </div>
       
       <div className='mt-16'>
-        <FormField />
+        <FormField
+          labelName='Search posts'
+          type='text'
+          name='text'
+          placeholder='Search posts'
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
       </div>
 
       <div className='mt-10'>
@@ -68,7 +91,7 @@ const Home = () => {
             <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
               {searchText ? (
                 <RenderCards 
-                  data={[]}
+                  data={searchedResults}
                   title='No search results found'
                 />
               ) : (
